2f0bfdc51a4608d3f36eec590246984243154f34
Jakob Fipke <jakobtaze@gmail.com>
2021-10-01 11:44:57 -0700

    Initial Commit

---
+++ Tap Tracker/Tap_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_TrackerApp.swift
+//  Tap Tracker
+//
+//  Created by Jakob Fipke on 2021-10-01.
+//
+
+import SwiftUI
+
+@main
+struct Tap_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
